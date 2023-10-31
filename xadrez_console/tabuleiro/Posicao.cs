6d9b4d08a588434ac6497272d00d2c1a79b0813c
Leastorga <leticia.trancozo@gmail.com>
2023-10-31 12:32:46 -0300

    Criando a classe posição

---
+++ xadrez_console/tabuleiro/Posicao.cs
@@ -0,0 +1,24 @@
+namespace tabuleiro
+{
+    public class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+        public Posicao()
+        {
+        }
+        public Posicao(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha
+            + ", "
+            + Coluna
+            ;
+        }
+    }
+}
