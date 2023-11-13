5e38ac750958dddfaca1d72cae6cc80757e355ce
Leastorga <leticia.trancozo@gmail.com>
2023-11-13 11:31:15 -0300

    Adicionando restrições de movimento na torre

--- xadrez_console/jogoDeXadrez/Torre.cs
+++ xadrez_console/jogoDeXadrez/Torre.cs
@@ -3,14 +3,83 @@ using tabuleiro;
 // Classe que representa a Peça Torre
 namespace jogoDeXadrez
 {
-    public class Torre : Peca
+    class Torre : Peca
     {
-        public Torre(Tabuleiro tabuleiro,Cor cor) : base(tabuleiro, cor)
+        public Torre(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
         {
         }
+
         public override string ToString()
         {
             return "T";
         }
+
+        // Método privado que possibilita a verificação de o rei pode ou não se mover. 
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tabuleiro.peca(pos); // P é igual a peça no tabuleiro em uma posição
+            return p != null || p.cor != cor; // retorne p se não for nula ou se houver uma peça adversária
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] matriz = new bool[tabuleiro.linhas, tabuleiro.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // Mover para cima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            while (tabuleiro.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.linha, pos.coluna] = true;
+                if (tabuleiro.peca(pos) != null && tabuleiro.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = -1;
+            }
+
+
+            // Mover para baixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            while (tabuleiro.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.linha, pos.coluna] = true;
+                if (tabuleiro.peca(pos) != null && tabuleiro.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = +1;
+            }
+
+
+            // Para direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            while (tabuleiro.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.linha, pos.coluna] = true;
+                if (tabuleiro.peca(pos) != null && tabuleiro.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = +1;
+            }
+
+            // Para esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            while (tabuleiro.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.linha, pos.coluna] = true;
+                if (tabuleiro.peca(pos) != null && tabuleiro.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = -1;
+            }
+            return matriz;
+        }
+
+
+
     }
 }
