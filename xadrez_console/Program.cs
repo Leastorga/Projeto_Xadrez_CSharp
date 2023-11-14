3cbb8e1fcecc57b9229125b7578064fe355a0701
Leastorga <leticia.trancozo@gmail.com>
2023-11-14 09:18:54 -0300

    Realizando a chamada da sobrecarga imprimir tabuleiro

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -23,13 +23,15 @@ namespace xadrez_console
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
 
+                    bool [,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
 
                     partida.executaMovimento(origem, destino);
                 }
-
-
             }
             catch (TabuleiroException e)
             {
