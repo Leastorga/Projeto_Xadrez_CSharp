33441a364a1182d3aa7c078845d6b3b09844d800
Leastorga <leticia.trancozo@gmail.com>
2023-11-14 09:18:18 -0300

    Adicionando sobrecarga para mudar o fundo nas posições possíveis das peças

--- xadrez_console/Tela.cs
+++ xadrez_console/Tela.cs
@@ -17,20 +17,34 @@ namespace xadrez_console
                 Console.Write(8 - i + " ");
                 for (int j = 0; j < tabuleiro.colunas; j++)
                 {
-                    if (tabuleiro.peca(i, j) == null) // se não houver peça no tabuleiro imprimeiro "-"
-                    {
-                        Console.Write("- ");
-                    }
-                    else
-                    {
-                        Tela.imprimirPeca(tabuleiro.peca(i, j));
-                        Console.Write(" "); // Se houver imprimir a peça
-                    }
+                    imprimirPeca(tabuleiro.peca(i, j));
                 }
                 Console.WriteLine(); // Quando acabar as colunas pula de linha
             }
-            Console.Write("  a b c d e f g h");
-            Console.WriteLine();
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void imprimirTabuleiro(Tabuleiro tabuleiro, bool[,] posicoesPossiveis)
+        {
+            ConsoleColor fundoOriginal = Console.BackgroundColor;
+            ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
+
+            for (int i = 0; i < tabuleiro.linhas; i++)
+            {
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < tabuleiro.colunas; j++)
+                {
+                    if(posicoesPossiveis[i,j]){
+                        Console.BackgroundColor = fundoAlterado;
+                    }else{
+                        Console.BackgroundColor = fundoOriginal;
+                    }
+                    imprimirPeca(tabuleiro.peca(i, j));
+                    Console.BackgroundColor = fundoOriginal;
+                }
+                Console.WriteLine(); // Quando acabar as colunas pula de linha
+            }
+            Console.WriteLine("  a b c d e f g h");
+            Console.BackgroundColor = fundoOriginal;
             
         }
 
@@ -38,21 +52,30 @@ namespace xadrez_console
         // Método de imprimir Peça, diferenciando as cores das peças. 
         public static void imprimirPeca(Peca peca)
         {
-            if (peca.cor == Cor.Branca)
+            if (peca == null)
             {
-                Console.Write(peca);
+                Console.Write("- ");
             }
             else
             {
-                ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                Console.Write(peca);
-                Console.ForegroundColor = aux;
+                if (peca.cor == Cor.Branca)
+                {
+                    Console.Write(peca);
+                }
+                else
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.Write(peca);
+                    Console.ForegroundColor = aux;
+                }
+                Console.Write(" ");
             }
         }
 
-// Método que le a posição escrita pelo o usuário, primeiro uma string que receberá a posição, onde primeiro será o char coluna e o int linha que está sendo convertido em uma string.
-        public static PosicaoXadrez lerPosicaoXadrez(){
+        // Método que le a posição escrita pelo o usuário, primeiro uma string que receberá a posição, onde primeiro será o char coluna e o int linha que está sendo convertido em uma string.
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
             string s = Console.ReadLine();
             char coluna = s[0];
             int linha = int.Parse(s[1] + " ");
