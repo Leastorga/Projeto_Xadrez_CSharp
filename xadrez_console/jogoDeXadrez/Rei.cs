135d74574ac1292f5152c45bf03fdff693726608
Leastorga <leticia.trancozo@gmail.com>
2023-11-21 11:23:43 -0300

    Implementando o método jogada especial roque grande

--- xadrez_console/jogoDeXadrez/Rei.cs
+++ xadrez_console/jogoDeXadrez/Rei.cs
@@ -104,6 +104,18 @@ namespace jogoDeXadrez
                     }
                 }
 
+            // #JogadaEspecial Roque Grande
+                Posicao posicaoTorre2 = new Posicao(Posicao.Linha, Posicao.Coluna - 4);
+                if(TesteTorreParaRoque(posicaoTorre2))
+                {
+                    Posicao p1 = new Posicao(Posicao.Linha, Posicao.Coluna - 1);
+                    Posicao p2 = new Posicao(Posicao.Linha, Posicao.Coluna - 2);
+                    Posicao p3 = new Posicao(Posicao.Linha, Posicao.Coluna - 3);
+                    if(Tab.Peca(p1) == null && Tab.Peca(p2) ==  null && Tab.Peca(p3) == null)
+                    {
+                        matriz[Posicao.Linha, Posicao.Coluna - 2] = true;
+                    }
+                }
             }
             return matriz;
         }
