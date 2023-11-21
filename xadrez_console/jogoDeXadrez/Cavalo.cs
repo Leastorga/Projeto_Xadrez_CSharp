d93fca6c09f32905084d57ef8711b83f4009d36d
Leastorga <leticia.trancozo@gmail.com>
2023-11-21 11:04:05 -0300

    Criando peça Cavalo

---
+++ xadrez_console/jogoDeXadrez/Cavalo.cs
@@ -0,0 +1,77 @@
+using tabuleiro;
+
+namespace jogoDeXadrez
+{
+    class Cavalo : Peca
+    {
+        public Cavalo(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "C";
+        }
+
+        private bool PodeMover(Posicao pos){
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis(){
+            bool[,] matriz = new bool [Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 2);
+            if(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+            
+            pos.DefinirValores(Posicao.Linha - 2, Posicao.Coluna - 1);
+            if(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+
+            pos.DefinirValores(Posicao.Linha - 2, Posicao.Coluna + 1);
+            if(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 2);
+            if(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 2);
+            if(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna]  = true;
+            }
+
+            pos.DefinirValores(Posicao.Linha + 2, Posicao.Coluna + 1);
+            if(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna]  = true;
+            }
+
+            pos.DefinirValores(Posicao.Linha + 2, Posicao.Coluna - 1);
+            if(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna]  = true;
+            }
+
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 2);
+            if(Tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna]  = true;
+            }
+
+            return matriz;
+        }
+    }
+}
