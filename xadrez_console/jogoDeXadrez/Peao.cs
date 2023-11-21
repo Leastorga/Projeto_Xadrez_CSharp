eacdb6a333a3463533267c99eee882823b648520
Leastorga <leticia.trancozo@gmail.com>
2023-11-21 11:04:35 -0300

    Criando peça Peao

---
+++ xadrez_console/jogoDeXadrez/Peao.cs
@@ -0,0 +1,90 @@
+
+using tabuleiro;
+
+namespace jogoDeXadrez
+{
+    class Peao : Peca
+    {
+        private PartidaDeXadrez Partida;
+
+        public Peao(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+
+        private bool ExisteInimigo(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p != null && p.Cor != Cor;
+        }
+
+        private bool Livre(Posicao pos)
+        {
+            return Tab.Peca(pos) == null;
+        }
+
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] matriz = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            if (Cor == Cor.Branca)
+            {
+                pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+                if (Tab.PosicaoValida(pos) && Livre(pos))
+                {
+                    matriz[pos.Linha, pos.Coluna] = true;
+                }
+
+                pos.DefinirValores(Posicao.Linha - 2, Posicao.Coluna);
+                Posicao p2 = new Posicao(Posicao.Linha - 1, Posicao.Coluna);
+                if(Tab.PosicaoValida(p2) && Livre(p2) && Tab.PosicaoValida(pos) && Livre(pos) && QteMovimentos == 0){
+                    matriz[pos.Linha, pos.Coluna] = true;   
+                }
+
+                pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+                if(Tab.PosicaoValida(pos) && ExisteInimigo(pos)){
+                    matriz[pos.Linha, pos.Coluna] = true;
+                }
+
+                pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+                if(Tab.PosicaoValida(pos) && ExisteInimigo(pos)){
+                    matriz[pos.Linha, pos.Coluna] = true;
+                }
+            }else
+            {
+                pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+                if(Tab.PosicaoValida(pos) && Livre(pos))
+                {
+                    matriz[pos.Linha, pos.Coluna] = true;
+                }
+
+                pos.DefinirValores(Posicao.Linha + 2, Posicao.Coluna);
+                Posicao p2 = new Posicao(Posicao.Linha + 1, Posicao.Coluna);
+                if(Tab.PosicaoValida(p2) && Livre(p2) && Tab.PosicaoValida(pos) && Livre(pos) && QteMovimentos == 0)
+                {
+                    matriz[pos.Linha, pos.Coluna] = true;
+                }
+
+                pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+                if(Tab.PosicaoValida(pos) && ExisteInimigo(pos))
+                {
+                    matriz[pos.Linha, pos.Coluna] = true;
+                }
+
+                pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna +1);
+                if(Tab.PosicaoValida(pos) && ExisteInimigo(pos))
+                {
+                    matriz[pos.Linha, pos.Coluna] = true;
+                }
+            }
+            return matriz;
+        }
+    }
+}
